245079639985a7ac6e4a28687f15b0c3283d64b2
Wei He <rosemary_hw@163.com>
2022-11-08 12:48:39 +0800

    项目创建

---
+++ backend/src/main/java/com/kob/backend/controller/test.java
@@ -0,0 +1,16 @@
+package com.kob.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test/")
+public class test {
+
+    @RequestMapping("index/")
+    public String index()
+    {
+        return "hello world!!!";
+    }
+}
